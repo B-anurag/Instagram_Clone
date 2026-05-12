9c47e55c3b04e5e7a1cb1dfec88ca42f9274b85b
Anurag Bharadwaj <159235279+B-anurag@users.noreply.github.com>
2026-05-12 16:59:17 +0530

    New features

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,5 @@
 const userModel = require('../models/user.model');
-const crypto = require('crypto');
+const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 const cookieParser = require('cookie-parser');
 
@@ -36,7 +36,7 @@ const cookieParser = require('cookie-parser');
         });
     }
 
-    const hash = crypto.createHash('sha256').update(password).digest('hex');
+    const hash = await bcrypt.hash(password, 10);
 
     const user = await userModel.create({
         username,
@@ -94,9 +94,7 @@ async function loginController(req, res){
         });
     }
 
-    const hash = crypto.createHash('sha256').update(password).digest('hex');
-    const isPasswordValid =  hash === user.password;
-
+        const isPasswordValid = await bcrypt.compare(password, user.password);
     if(!isPasswordValid) {
         return res.status(401).json({
             message: 'Invalid password'
